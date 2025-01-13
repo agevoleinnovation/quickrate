bfe7705c753063054b3caa6e43fff77a2f2e278a
nidhijariwala28 <jariwalanidhi28@gmail.com>
2025-01-13 11:44:10 +0530

    changes by nidhi jariwala at 13 jan 25

--- db.php
+++ db.php
@@ -2,13 +2,14 @@
 //db.php
 // Database configuration
 $servername = "localhost";
-$username = "u408770847_quickrate";
-$password = "Agevole@wbp409";
-$database = "u408770847_quickrate";
+$username = "root";
+$password = "";
+$database = "myalbumry";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
